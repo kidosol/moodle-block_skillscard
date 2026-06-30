8ed045df6a70f9d47e38e9f8451b7884a5b121e6
brandonchoo05 <76644728+kidosol@users.noreply.github.com>
2026-06-30 16:25:54 +0800

    Update block_skillscard.php

--- block_skillscard.php
+++ block_skillscard.php
@@ -27,14 +27,13 @@
 /**
  * Skills Card block implementation.
  */
-class block_skillscard extends block_base
-{
+class block_skillscard extends block_base {
+
     /**
      * Initialise the block title.
      */
-    public function init()
-    {
-        $this->title = get_string("skillscard", "block_skillscard");
+    public function init() {
+        $this->title = get_string('skillscard', 'block_skillscard');
     }
 
     /**
@@ -42,8 +41,7 @@ class block_skillscard extends block_base
      *
      * @return bool
      */
-    public function instance_allow_multiple()
-    {
+    public function instance_allow_multiple() {
         return true;
     }
 
@@ -52,79 +50,83 @@ class block_skillscard extends block_base
      *
      * @return stdClass|null
      */
-    public function get_content()
-    {
+    public function get_content() {
         global $USER, $DB;
 
         if ($this->content !== null) {
             return $this->content;
         }
 
-        $id = optional_param("id", 0, PARAM_INT);
-        $user = $USER;
+        $userid = optional_param('id', $USER->id, PARAM_INT);
 
-        // Load user.
-        if (is_siteadmin() && $id) {
-            $user = $DB->get_record("user", ["id" => $id], "*", MUST_EXIST);
-        } elseif ($id) {
-            return;
+        // Security check: Only admins can view other people's cards.
+        if ($userid != $USER->id && !is_siteadmin()) {
+            return null;
         }
 
         $this->content = new stdClass();
-        $this->content->text = "";
-        $this->content->footer = "";
+        $this->content->text = '';
+        $this->content->footer = '';
 
-        // Get data.
-        $sql = "SELECT mc.id, COALESCE(c.scaleid, cf.scaleid, 0) AS scaleidx, c.shortname as compname, mc.grade as grade
+        // Optimized SQL: Join the scale table here so we don't query inside the loop.
+        $sql = "SELECT mc.id, 
+                       c.shortname AS compname, 
+                       mc.grade, 
+                       s.scale AS scalerecord
                   FROM {competency_usercomp} mc
-                  JOIN {user} mu on mu.id = mc.userid
-                  JOIN {competency} c on c.id = mc.competencyid
-             LEFT JOIN {competency_framework} cf on cf.id = c.competencyframeworkid
+                  JOIN {competency} c ON c.id = mc.competencyid
+             LEFT JOIN {competency_framework} cf ON cf.id = c.competencyframeworkid
+             LEFT JOIN {scale} s ON s.id = COALESCE(c.scaleid, cf.scaleid)
                  WHERE mc.userid = :userid
-              ORDER BY mc.userid";
+              ORDER BY c.shortname ASC";
 
-        $skillscard = $DB->get_records_sql($sql, ["userid" => $user->id]);
+        $skillscard = $DB->get_records_sql($sql, ['userid' => $userid]);
+
         if (empty($skillscard)) {
-            $this->content->text = get_string(
-                "noskillscard",
-                "block_skillscard"
-            );
+            $this->content->text = get_string('noskillscard', 'block_skillscard');
             return $this->content;
         }
 
-        // Render data.
-        $items = "";
+        $items = '';
         foreach ($skillscard as $sc) {
-            $values = $DB->get_field("scale", "scale", ["id" => $sc->scaleidx]);
-            $scales = $values ? explode(",", $values) : [];
-            $grade = $scales[$sc->grade - 1] ?? "";
-            $skill = format_text($sc->compname, FORMAT_PLAIN);
-
-            $icon = html_writer::tag("i", "", [
-                "class" => "fa fa-trophy fa-5x text-primary",
-                "style" => "float: left; padding: 10px;",
-            ]);
-            $content =
-                html_writer::tag("span", $icon) . html_writer::empty_tag("br");
-            if ($grade !== "") {
-                $content .=
-                    get_string("rank", "block_skillscard") .
-                    " " .
-                    s($grade) .
-                    html_writer::empty_tag("br");
+            $grade = '';
+            if (!empty($sc->scalerecord) && !empty($sc->grade)) {
+                $scales = explode(',', $sc->scalerecord);
+                // Scales in Moodle are 1-indexed.
+                $grade = $scales[$sc->grade - 1] ?? '';
             }
-            $content .=
-                get_string("competency", "block_skillscard") . " " . $skill;
 
-            $items .= html_writer::tag("li", $content, [
-                "style" => "text-align: center;",
+            $skillname = format_string($sc->compname);
+
+            // Icon with Bootstrap padding and float classes.
+            $icon = html_writer::tag('i', '', [
+                'class' => 'fa fa-trophy fa-5x text-primary float-left p-2',
             ]);
-            $items .= html_writer::tag("div", "", ["style" => "clear: both;"]);
+
+            $content = html_writer::span($icon);
+            $content .= html_writer::empty_tag('br');
+
+            if ($grade !== '') {
+                $content .= html_writer::tag('div', 
+                    get_string('rank', 'block_skillscard') . ' ' . s($grade), 
+                    ['class' => 'font-weight-bold']
+                );
+            }
+
+            $content .= html_writer::tag('div', 
+                get_string('competency', 'block_skillscard') . ' ' . $skillname
+            );
+
+            // Item wrapper using Bootstrap 'text-center' and 'clearfix'.
+            $items .= html_writer::tag('li', $content, [
+                'class' => 'text-center clearfix mb-3',
+            ]);
         }
 
-        $this->content->text .= html_writer::tag("ul", $items, [
-            "style" => "list-style: none;",
+        $this->content->text = html_writer::tag('ul', $items, [
+            'class' => 'list-unstyled',
         ]);
+
         return $this->content;
     }
 }
